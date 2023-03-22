7664d805ac9422a0e8727a974d486e43d7dd6267
Matt McGeown <matt_live@live.co.uk>
2023-03-22 02:27:34 +0000

    update file name and import

--- src/utils/trpc.ts
+++ src/utils/trpc.ts
@@ -1,6 +1,6 @@
 import { httpBatchLink } from '@trpc/client';
 import { createTRPCNext } from '@trpc/next';
-import type { AppRouter } from '@/backend/routers/_app';
+import type { AppRouter } from '@/backend/routers';
 
 function getBaseUrl() {
   if (typeof window !== 'undefined')
