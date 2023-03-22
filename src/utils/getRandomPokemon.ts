c837ef52d935f244203748ec4e170375cdfd4f85
Matt McGeown <matt_live@live.co.uk>
2023-03-22 01:28:04 +0000

    adding random pokemon id fetch

---
+++ src/utils/getRandomPokemon.ts
@@ -0,0 +1,18 @@
+const MAX_DEX_ID = 5;
+
+export const getRandomPokemon: (shouldSkip?: number) => number = (
+  shouldSkip?: number
+) => {
+  const pokedexNumber = Math.floor(Math.random() * MAX_DEX_ID) + 1;
+
+  if (pokedexNumber !== shouldSkip) return pokedexNumber;
+
+  return getRandomPokemon(shouldSkip);
+};
+
+export const getOptionsForVote = () => {
+  const firstId = getRandomPokemon();
+  const secondId = getRandomPokemon(firstId);
+
+  return [firstId, secondId];
+};
